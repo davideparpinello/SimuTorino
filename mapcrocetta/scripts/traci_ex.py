d875d3d9fc5b92079c1a39456e9befcd4a2d3f0d
Davide Zordan <davide.zordan@studenti.unitn.it>
2022-02-03 17:10:25 +0100

    counter

--- mapcrocetta/scripts/traci_ex.py
+++ mapcrocetta/scripts/traci_ex.py
@@ -24,13 +24,14 @@ traffic_file_url = "https://opendata.5t.torino.it/get_fdt"
 xml_node_elem = 'FDT_data'
 
 namespaces = {'': 'https://simone.5t.torino.it/ns/traffic_data.xsd'}
-
+counter=0
 step=0
 time=100
 traci.start(sumoCmd)
 while traci.simulation.getMinExpectedNumber() > 0 :
     traci.simulationStep()
-    if (step % time)<0.1:
+    
+    if (step % time)<0.1:     #every "time"(100) seconds of simulation
         
         response = requests.get(traffic_file_url)
         string_xml = response.content
@@ -39,7 +40,7 @@ while traci.simulation.getMinExpectedNumber() > 0 :
         with open(calibrators_db_file, "r") as calibrators_db:
             calibrators = json.load(calibrators_db)
             i=1
-            print("\nSynchronizing " + str(i) + " current flow with opendata")
+            print("\nSynchronizing " + str(counter) + " current flow with opendata")
             for calibrator in calibrators['calibrators']:
                 current_lat = calibrator['pos']['lat']
                 current_lon = calibrator['pos']['lon']
@@ -51,6 +52,7 @@ while traci.simulation.getMinExpectedNumber() > 0 :
                         print("calibrator_"+calibrator['id'] + " " + current_flow)
                         traci.calibrator.setFlow("calibrator_"+calibrator['id'], 0, 3600, current_flow, 10, 'DEFAULT_VEHTYPE' , "routedistcal"+str(i), departLane='free', departSpeed='max')
                 i+=1
+    counter+=1
     step += 0.1
 traci.close()
 

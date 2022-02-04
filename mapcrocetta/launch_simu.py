6ff038043df4dd74b5568995965a152b39592dcb
Davide Zordan <davide.zordan@studenti.unitn.it>
2022-02-04 10:13:36 +0100

    added selenium

--- mapcrocetta/launch_simu.py
+++ mapcrocetta/launch_simu.py
@@ -12,11 +12,14 @@ os.system("python3 scripts/generate_cal_routes.py")
 print("\nGenerating calibrator additional file to be opened with Sumo..")
 os.system("python3 scripts/generate_calibrators_file.py")
 
+print("\nDownloading gNodes opendata through Selenium script..")
+os.system("python3 scripts/selenium_autologin.py")
+
 print("\nGenerating gnodeB positions for Omnet simulation..")
 os.system("python3 scripts/generate_pos_gnodes.py")
 
 print("\nConverting .ned config file in .ned.xml to be manipulated..")
-os.system("opp_nedtool c utils/Crocetta.ned -o utils/Crocetta.ned.xml") #NB ricorda di cambiare nome file
+os.system("opp_nedtool c utils/Crocetta.ned -o utils/Crocetta.ned.xml") 
 
 print("\nModifying ned.xml network file..")
 os.system("python3 scripts/changeNEDfile.py")
